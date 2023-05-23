9c7613be015c48bf0d1de2c9553ba0666171b944
Nicolas Moises <nicolas.nicc21@gmail.com>
2023-05-23 18:57:56 -0300

    fix. remove files of plan page not find on module

--- src/pages/Plan/index.tsx
+++ src/pages/Plan/index.tsx
@@ -1,12 +1,9 @@
-import { CTA } from "../../components/CTA";
 import { Faq } from "./components/Faq";
-import { Pricing } from "./components/Pricing";
 import { BlurPlan, PlanContainer } from "./styles";
 
 export function Plan() {
     return (
         <PlanContainer>
-            <Pricing />
             <Faq />
             <BlurPlan />
         </PlanContainer>
